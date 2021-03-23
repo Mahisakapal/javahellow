8d1960205904e8ffa947c629c19d979599463c63
Mahisakapal <mahisakapal@gmail.com>
2021-03-23 20:24:14 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("This is Amardip"); 
-        System.out.println("lets try devop"); 
+        System.out.println("lets try devops"); 
+        System.out.println("try of pull scm"); 
     }
 }
