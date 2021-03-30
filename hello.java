a7777d5262dd752c16c3111e733115735d799d9d
Mahisakapal <mahisakapal@gmail.com>
2021-03-30 19:50:55 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -8,6 +8,6 @@ class hello{
         System.out.println("one more added"); 
         System.out.println("testing"); 
         System.out.println("webhook empliment "); 
-        System.out.println("webhook empliment 2 ");
+        System.out.println("webhook empliment 1 ");
     }
 }
